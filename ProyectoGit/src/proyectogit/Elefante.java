eee0c31ad767320328529ffb791acb43996c4245
Roman Enrich <roman4004@hotmail.com>
2021-10-14 11:04:44 -0300

    actualizacion de Elefante

--- ProyectoGit/src/proyectogit/Elefante.java
+++ ProyectoGit/src/proyectogit/Elefante.java
@@ -2,19 +2,11 @@ package proyectogit;
 
 
 public class Elefante {
-<<<<<<< HEAD
 
 private String nombre;
 private String raza;
 private int edad;
 private String origen;
-=======
-    
 
-    private String nombre;
-    private String raza;
-    private int edad;
-    private String origen;
->>>>>>> rama_roman
 
 }

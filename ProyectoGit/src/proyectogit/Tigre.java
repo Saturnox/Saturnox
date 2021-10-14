a8a95ae809b9bf29cad9fb07404da6d92daefbb0
leonahuelfrias <leonahuelfrias@gmail.com>
2021-10-14 12:29:54 -0300

    primer commit Nahuel Frias

---
+++ ProyectoGit/src/proyectogit/Tigre.java
@@ -0,0 +1,16 @@
+package proyectogit;
+
+import java.util.Date;
+
+/**
+ *
+ * @author leonahuel
+ */
+public class Tigre {
+
+    private String nombre;
+    private String raza;
+    private Date edad;
+    private String origen;
+
+}

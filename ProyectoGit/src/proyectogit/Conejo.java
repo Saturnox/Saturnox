eb00536e636748fa7d884a396602f2b2eefcef60
Vale Riveros <valeriveros86@gmail.com>
2021-10-17 00:44:27 -0300

    Agrego atributo rama_vale

--- ProyectoGit/src/proyectogit/Conejo.java
+++ ProyectoGit/src/proyectogit/Conejo.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectogit;
 
 /**
@@ -10,5 +6,9 @@ package proyectogit;
  * @author river
  */
 public class Conejo {
+    private String nombre;
+    private int edad;
+    private String raza;
+    private String apodo;
     
 }
